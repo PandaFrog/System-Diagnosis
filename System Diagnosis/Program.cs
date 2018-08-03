6de9064af5da7efc67b08a3d485858f091b978cc
Brad Glew <bg619316@gmail.com>
2018-08-03 12:29:17 +0800

    Cleaned up some code

---
+++ System Diagnosis/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Diagnostics;
+using System.Threading;
+
+namespace System_Diagnosis
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            // Counters for grabbing system info
+            PerformanceCounter perfCpupercent = new PerformanceCounter("Processor Information", "% Processor Time", "_Total");
+            PerformanceCounter perfMem = new PerformanceCounter("Memory", "Available Mbytes");
+            PerformanceCounter perfCDrive = new PerformanceCounter("LogicalDisk", "% Free Space", "C:");
+            PerformanceCounter perfDDrive = new PerformanceCounter("LogicalDisk", "% Free Space", "D:");
+            PerformanceCounter perfEDrive = new PerformanceCounter("LogicalDisk", "% Free Space", "D:");
+
+            while (true)
+            {
+                //Writes next value of system info
+                Console.WriteLine("{0}Mb Ram is free", perfMem.NextValue());
+                Console.WriteLine("CPU is load is {0}%", perfCpupercent.NextValue());
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine("C Drive has has {0}% free space", perfCDrive.NextValue());
+                Console.WriteLine("D Drive has has {0}% free space", perfDDrive.NextValue());
+                Console.WriteLine("E Drive has has {0}% free space", perfEDrive.NextValue());
+                Console.WriteLine();
+                Console.WriteLine();
+                Console.WriteLine();
+
+                Thread.Sleep(1000);
+            }
+
+
+
+
+
+
+        }
+    }
+}
---
+++ System Diagnosis/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("System Diagnosis")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("System Diagnosis")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("1c888848-d3f1-4666-8350-a2b771c4890a")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Build and Revision Numbers
+// by using the '*' as shown below:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
